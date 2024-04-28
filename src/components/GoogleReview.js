3615302bcfee792db18f74fbe49fecc47d017b45
tarungajamoni <saitarun.goud1998@gmail.com>
2024-04-28 17:20:22 +0530

    deploy3

--- src/components/GoogleReview.js
+++ src/components/GoogleReview.js
@@ -4,7 +4,7 @@ import {ElfsightWidget} from 'react-elfsight-widget';
 export default function GoogleReview() {
     return (
     <section className="section" id="reviews">
-        <ElfsightWidget widgetId='cdc691ba-4762-4572-a5c8-546a2040c997' lazy/>;
+        <ElfsightWidget widgetId='cdc691ba-4762-4572-a5c8-546a2040c997' lazy/>
         </section>
     )
 }
